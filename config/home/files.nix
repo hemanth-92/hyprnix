e4e432009ba61028a937c19862b70140b805f23c
hemanth-92 <hemanth9679@gmail.com>
2024-03-20 07:13:21 +0530

    removed

--- config/home/files.nix
+++ config/home/files.nix
@@ -4,8 +4,6 @@
   # Place Files Inside Home Directory
   home.file.".emoji".source = ./files/emoji;
   home.file.".base16-themes".source = ./files/base16-themes;
-  home.file.".face".source = ./files/face.jpg; # For GDM
-  home.file.".face.icon".source = ./files/face.jpg; # For SDDM
   home.file.".config/rofi/rofi.jpg".source = ./files/rofi.jpg;
   home.file.".config/starship.toml".source = ./files/starship.toml;
   home.file.".config/swaylock-bg.jpg".source = ./files/media/swaylock-bg.jpg;
