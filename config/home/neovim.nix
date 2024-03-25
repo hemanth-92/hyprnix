4582055aa275b1bf410f3f979784c21db5ab7185
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-03-25 18:58:13 +0530

    mappings for cmp

--- config/home/neovim.nix
+++ config/home/neovim.nix
@@ -69,11 +69,6 @@ in
       nvim-autopairs.enable = true;
       nix.enable = true;
       comment-nvim.enable = true;
-      cmp-nvim-lsp.enable = true;
-      cmp-nvim-lsp-document-symbol.enable = true;
-      cmp-nvim-lsp-signature-help.enable = true;
-      cmp-nvim-lua.enable = true;
-      cmp_luasnip.enable = true;
       lualine = {
         enable = true;
       };
@@ -114,7 +109,8 @@ in
       };
       cmp = {
         enable = true;
-        settings = {          
+        settings = {
+        autoEnableSources = true;
         sources = [
             { name = "nvim_lsp"; }
             { name = "nvim_lsp_document_symbol"; }
@@ -126,6 +122,53 @@ in
             function(args)
               require('luasnip').lsp_expand(args.body)
             end
+          '';
+        };
+        extraOptions.mapping = {
+          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C+c>" = "cmp.mapping.abort()";
+          "<Esc>" = "cmp.mapping.abort()";
+          "<CR>" = "cmp.mapping.confirm({ select = false })";
+
+          "<C-p>" = "cmp.mapping.select_prev_item()";
+          "<C-n>" = "cmp.mapping.select_next_item()";
+
+          "<Up>" = "cmp.mapping.select_prev_item()";
+          "<Down>" = "cmp.mapping.select_next_item()";
+
+          "<C-Space>" = "cmp.mapping.complete({})";
+
+          "<Tab>" = ''
+            cmp.mapping(
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif require("luasnip").expand_or_locally_jumpable() then
+                  require("luasnip").expand_or_jump()
+                elseif has_words_before() then
+                  cmp.complete()
+                else
+                  fallback()
+                end
+              end,
+              {"i", "s"}
+            )
+          '';
+
+          "<S-Tab>" = ''
+            cmp.mapping(
+            function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif require("luasnip").jumpable(-1) then
+                require("luasnip").jump(-1)
+              else
+                fallback()
+              end
+            end,
+            {"i", "s"}
+            )
           '';
         };
       };
