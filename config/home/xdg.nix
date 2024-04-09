c710b021c0c45a71e96af77830082433754a2405
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-04-09 14:38:37 +0530

    xdg

---
+++ config/home/xdg.nix
@@ -0,0 +1,72 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  imageViewer = "org.gnome.Loupe.desktop";
+  mediaPlayer = "mpv.desktop";
+  browser = "firefox.desktop";
+  docOpener = "zathura.desktop";
+  fileManager = "org.gnome.Nautilus.desktop";
+  torrentClient = "transmission-gtk.desktop";
+  textEditor = "neovim.desktop";
+in {
+  home.packages = [pkgs.xdg-utils];
+  xdg = {
+    enable = true;
+    portal = with pkgs; {
+      enable = true;
+      configPackages = [xdg-desktop-portal-gtk xdg-desktop-portal-hyprland xdg-desktop-portal];
+      extraPortals = [xdg-desktop-portal-gtk xdg-desktop-portal];
+      xdgOpenUsePortal = true;
+    };
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      desktop = "${config.home.homeDirectory}/Projects";
+      publicShare = "${config.home.homeDirectory}/.local/public";
+      templates = "${config.home.homeDirectory}/.local/templates";
+    };
+
+    mimeApps = rec {
+      enable = true;
+      associations.added = defaultApplications;
+      defaultApplications = {
+        "application/pdf" = [docOpener];
+        "inode/directory" = [fileManager];
+        "x-scheme-handler/magnet" = torrentClient;
+
+        # Web stuff
+        "text/html" = [browser];
+        "x-scheme-handler/http" = browser;
+        "x-scheme-handler/https" = browser;
+        "application/xhtml+xml" = browser;
+
+        # Images
+        "image/jpeg" = imageViewer;
+        "image/avif" = imageViewer;
+        "image/gif" = imageViewer;
+        "image/jpg" = imageViewer;
+        "image/pjpeg" = imageViewer;
+        "image/png" = imageViewer;
+        "image/tiff" = imageViewer;
+        "image/webp" = imageViewer;
+        "image/x-bmp" = imageViewer;
+        "image/x-gray" = imageViewer;
+        "image/x-icb" = imageViewer;
+        "image/x-ico" = imageViewer;
+        "image/x-png" = imageViewer;
+
+        # Text & Code
+        "text/english" = textEditor;
+        "text/plain" = textEditor;
+        "application/x-shellscript" = textEditor;
+
+        # Videos
+        "video/webm" = mediaPlayer;
+        "video/mp4" = mediaPlayer;
+        "video/mkv" = mediaPlayer;
+      };
+    };
+  };
+}
