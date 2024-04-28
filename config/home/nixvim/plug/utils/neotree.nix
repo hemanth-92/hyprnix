a847134a38d921f7eec4bce4365165d6a75c103c
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-04-28 15:05:09 +0530

    neotree

--- config/home/nixvim/plug/utils/neotree.nix
+++ config/home/nixvim/plug/utils/neotree.nix
@@ -1,14 +1,12 @@
-{...}: {
+{
   programs.nixvim = {
     plugins.neo-tree = {
       enable = true;
-      popupBorderStyle = "rounded";
-      filesystem.hijackNetrwBehavior = "open_current";
     };
     keymaps = [
       {
         action = "<cmd>Neotree toggle<CR>";
-        key = "<leader>b";
+        key = "<leader>e";
         mode = "n";
         options = {
           silent = true;
