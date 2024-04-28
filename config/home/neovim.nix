43a163cac646844eeba7ef2789997b2477065970
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-04-28 08:30:24 +0530

    dnt know

--- config/home/neovim.nix
+++ config/home/neovim.nix
@@ -75,6 +75,36 @@ in
         };
         lsp-format.enable = true;
         surround.enable = true;
+        dap = {
+          enable = true;
+          adapters = {};
+          signs = {
+            dapBreakpoint = {
+              text = "●";
+              texthl = "DapBreakpoint";
+            };
+            dapBreakpointCondition = {
+              text = "●";
+              texthl = "DapBreakpointCondition";
+            };
+            dapLogPoint = {
+              text = "◆";
+              texthl = "DapLogPoint";
+            };
+          };
+          extensions = {
+            dap-go = {
+              enable = true;
+              delve.path = "${pkgs.delve}/bin/dlv";
+            };
+            dap-ui = {
+              enable = true;
+            };
+            dap-virtual-text = {
+              enable = true;
+            };
+          };
+        };
         luasnip = {
           enable = true;
           extraConfig = {
@@ -379,6 +409,21 @@ in
         options = {
           desc = "Toggle Tree View.";
         }; 
+      }
+      {
+        mode = "n";
+        key = "<leader>du";
+        action = "<cmd>lua require('dapui').toggle()<CR>";
+        options = {
+          silent = true;
+          desc = "Toggle dapui";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>tb";
+        action = "<cmd>lua require'dap'.toggle_breakpoint()<CR>";
+        options.silent = true;
       }
     ];
   };
