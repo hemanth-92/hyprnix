f458733acb2c6f01e40ec86026f6476146592f98
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-05-14 08:02:06 +0530

    edited

--- config/system/services.nix
+++ config/system/services.nix
@@ -1,18 +1,20 @@
-{ pkgs, config, lib, ... }:
-
-{
+{pkgs, ...}: {
   # List services that you want to enable:
-  services.openssh.enable = true;
-  services.fstrim.enable = true;
+  services = {
+    openssh.enable = true;
+    fstrim.enable = true;
+  };
   xdg.portal = {
     enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk
-    pkgs.xdg-desktop-portal
-  ];
-  configPackages = [ pkgs.xdg-desktop-portal-gtk
-  pkgs.xdg-desktop-portal-hyprland
-  pkgs.xdg-desktop-portal
-];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal
+    ];
+    configPackages = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal
+    ];
   };
   services.pipewire = {
     enable = true;
@@ -25,11 +27,13 @@
         {
           name = "libpipewire-module-protocol-pulse";
           args = {
-            pulse.min.req = "32/48000";
-            pulse.default.req = "32/48000";
-            pulse.max.req = "32/48000";
-            pulse.min.quantum = "32/48000";
-            pulse.max.quantum = "32/48000";
+            pulse = {
+              min.req = "32/48000";
+              default.req = "32/48000";
+              max.req = "32/48000";
+              min.quantum = "32/48000";
+              max.quantum = "32/48000";
+            };
           };
         }
       ];
@@ -40,19 +44,21 @@
     };
   };
 
-  hardware.pulseaudio.enable = false;
+  hardware = {
+    pulseaudio.enable = false;
+    bluetooth.enable = true; # enables support for Bluetooth
+    bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  };
   sound.enable = true;
   security.rtkit.enable = true;
   programs.thunar.enable = true;
   services.gvfs.enable = true;
   services.tumbler.enable = true;
-  services.gnome.gnome-keyring.enable=true;
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  services.gnome.gnome-keyring.enable = true;
   services.blueman.enable = true;
   security.pam.services.swaylock = {
     text = ''
-    auth include login
+      auth include login
     '';
   };
 }
