3d0b63e184a0369cde85cabe2b3db29f3b3a0bff
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-06-28 17:26:51 +0530

    changes

--- config/system/boot.nix
+++ config/system/boot.nix
@@ -1,20 +1,19 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{ inputs, pkgs, ... }: {
   boot = {
-    loader.grub = {
-      enable = true;
-      device = "nodev";
-      efiSupport = true;
-      useOSProber = true;
-      theme = inputs.catppuccin-grub + "/src/catppuccin-mocha-grub-theme";
-    };
     kernelPackages = pkgs.linuxPackages_zen;
-    loader.efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot";
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub = {
+        enable = true;
+        devices = [ "nodev" ];
+        efiSupport = true;
+        useOSProber = true;
+        theme = inputs.catppuccin-grub + "/src/catppuccin-mocha-grub-theme";
+      };
     };
+    supportedFilesystems.ntfs = true;
   };
 }
