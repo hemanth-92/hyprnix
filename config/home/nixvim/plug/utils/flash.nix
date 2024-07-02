6423312590ee293a5599d91884f07192cb32eefc
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-07-02 15:36:47 +0530

    trace errors

--- config/home/nixvim/plug/utils/flash.nix
+++ config/home/nixvim/plug/utils/flash.nix
@@ -2,24 +2,31 @@
   programs.nixvim = {
     plugins.flash = {
       enable = true;
-      labels = "asdfghjklqwertyuiopzxcvbnm";
-      search = {
-        mode = "fuzzy";
-      };
-      jump = {
-        autojump = true;
-      };
-      label = {
-        uppercase = false;
-        rainbow = {
-          enabled = false;
-          shade = 5;
+      settings = {
+        labels = "asdfghjklqwertyuiopzxcvbnm";
+        label = {
+          uppercase = false;
+          rainbow = {
+            enabled = false;
+            shade = 5;
+          };
         };
+        search = {
+          mode = "fuzzy";
+        };
+        jump = {
+          autojump = true;
+        };
+
       };
     };
     keymaps = [
       {
-        mode = ["n" "x" "o"];
+        mode = [
+          "n"
+          "x"
+          "o"
+        ];
         key = "s";
         action = "<cmd>lua require('flash').jump()<cr>";
         options = {
@@ -28,7 +35,11 @@
       }
 
       {
-        mode = ["n" "x" "o"];
+        mode = [
+          "n"
+          "x"
+          "o"
+        ];
         key = "S";
         action = "<cmd>lua require('flash').treesitter()<cr>";
         options = {
@@ -46,7 +57,10 @@
       }
 
       {
-        mode = ["x" "o"];
+        mode = [
+          "x"
+          "o"
+        ];
         key = "R";
         action = "<cmd>lua require('flash').treesitter_search()<cr>";
         options = {
--- config/home/packages.nix
+++ config/home/packages.nix
@@ -1,6 +1,13 @@
-{ pkgs, config, username, ... }:
-let inherit (import ../../options.nix) wallpaperDir wallpaperGit flakeDir;
-in {
+{
+  pkgs,
+  config,
+  username,
+  ...
+}:
+let
+  inherit (import ../../options.nix) wallpaperDir wallpaperGit flakeDir;
+in
+{
   # Install Packages For The User
   home.packages = with pkgs; [
     libvirt
@@ -10,7 +17,7 @@ in {
     gnome.file-roller
     swaynotificationcenter
     imv
-    transmission-gtk
+    transmission_4-gtk
     mpv
     rustup
     audacity
