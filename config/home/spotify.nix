f7268ea69f7671c9e23e6f72841b81b0f8bf55da
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-07-16 14:40:55 +0530

    fix spectify

--- config/home/spotify.nix
+++ config/home/spotify.nix
@@ -1,32 +1,13 @@
+{ pkgs, inputs, ... }:
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+in
 {
-  pkgs,
-  inputs,
-  config,
-  ...
-}: let
-  spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
-in {
-  imports = [inputs.spicetify-nix.homeManagerModule];
+  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
   programs.spicetify = {
     enable = true;
     theme = spicePkgs.themes.catppuccin;
     colorScheme = "mocha";
-
-    customColorScheme = with config.colorScheme.palette; {
-      accent = base0B;
-      accent-active = base0B;
-      accent-inactive = base01;
-      banner = base0B;
-      border-active = base0B;
-      border-inactive = base02;
-      header = base03;
-      highlight = base03;
-      main = base01;
-      notification = base0D;
-      notification-error = base08;
-      subtext = base04;
-      text = base05;
-    };
     enabledExtensions = with spicePkgs.extensions; [
       autoSkipVideo
       shuffle
@@ -34,9 +15,7 @@ in {
       seekSong
       adblock
       fullAppDisplay
-    ];
-    enabledCustomApps = with spicePkgs.apps; [
-      lyrics-plus
+      beautifulLyrics
     ];
   };
 }
